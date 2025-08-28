8b7815cad9106398e69a6f0e585c58e0e4f8c08d
Lucas <lucas22nic@gmail.com>
2025-08-27 22:24:16 -0300

    Se creo el modelo MVC Web

---
+++ T-Electronic/T-Electronic.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T_Electronic.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
