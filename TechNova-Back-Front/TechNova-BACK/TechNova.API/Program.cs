fde31931bb1bc5f37260e015019cff20f4cc2c7b
Lucas <lucas22nic@gmail.com>
2025-09-05 00:54:40 -0300

    Se agrego la coneccion a la base de datos y origenes permitidos, se habilitaron las politicas de cors

--- TechNova-Back-Front/TechNova-BACK/TechNova.API/Program.cs
+++ TechNova-Back-Front/TechNova-BACK/TechNova.API/Program.cs
@@ -1,10 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using TechNova.Logic.Data;
+
+//Definir nombre politica cors
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Configurar Servicio de DataBase
+builder.Services.AddDbContext<TechNovaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Leer origins permitidos
+var origenesPermitidos = builder.Configuration.GetValue<String>("OrigenesPermitidos")!.Split(',');
+
+//Add servicios Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +37,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Enrruta las solicitudes a los controladores correctos
+app.UseRouting();
+
+//Habilita politica cors
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
