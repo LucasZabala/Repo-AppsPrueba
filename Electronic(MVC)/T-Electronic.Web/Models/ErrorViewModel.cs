96c7c1657d4c1e26a44fa5f0102466370e86c300
Lucas <lucas22nic@gmail.com>
2025-09-06 11:39:47 -0300

    Se instalo Angular

--- Electronic(MVC)/T-Electronic.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace T_Electronic.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
